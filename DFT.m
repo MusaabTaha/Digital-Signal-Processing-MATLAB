b3539c1b41e10b6974d4c2e4dbc1ea757db1c750
MusaabTaha <104390876+MusaabTaha@users.noreply.github.com>
2023-12-18 13:58:31 +0100

    Update DFT.m

--- DFT.m
+++ DFT.m
@@ -37,3 +37,25 @@ function calcDFT()
 
 end
 
+
+function X=myDFT(x,N)
+    
+    L=length(x);
+
+    if(N<L)
+       error('N must be greater that or equal to length of x')
+    end
+
+    x = [x zeros(1,N-L)];
+
+    X = zeros(1,N);
+
+    for k=0:N-1
+        %%for 
+        n=[0:N-1];
+            X(k+1)=sum(x(n+1).*exp(-j*2*pi*k*n/N));
+        %end
+    end
+
+    end
+
